beeef4275ff6110dace168ca194b90d5bfafc75e
lxxself <lxxself@foxmail.com>
2020-03-16 18:52:35 +0800

    Update install_gost.sh

--- install_gost.sh
+++ install_gost.sh
@@ -1,8 +1,7 @@
 #/bin/sh
 mkdir gost
 cd gost
-wget https://github.com/ginuerzh/gost/releases/download/v2.11.0/gost-linux-amd64-2.11.0.gz
-gzip gost-linux-amd64-2.11.0.gz -d
-mv gost-linux-amd64-2.11.0 gost
+wget -O gost.gz https://github.com/ginuerzh/gost/releases/download/v2.11.0/gost-linux-amd64-2.11.0.gz
+gzip gost.gz -d
 chmod 777 gost
 ln gost /usr/bin/gost
